ceeac2c88a626ac5eaa19a88b8980f1c57a0bf16
Tatyana Korosteleva <tvkorosteleva@avito.ru>
2017-05-26 00:02:34 +0300

    add screen switching

---
+++ js/main.js
@@ -0,0 +1,72 @@
+(function () {
+  const mainContainerNode = document.querySelector(`main.central`);
+  const screenTemplates = [
+    document.querySelector(`#greeting`),
+    document.querySelector(`#rules`),
+    document.querySelector(`#game-1`),
+    document.querySelector(`#game-2`),
+    document.querySelector(`#game-3`),
+    document.querySelector(`#stats`)
+  ];
+  const LAST_SCREEN_INDEX = screenTemplates.length - 1;
+  let currentScreen = 0;
+
+  const KEY_CODES = {
+    arrowRight: 39,
+    arrowLeft: 37,
+    alt: 18
+  };
+
+  showScreen(currentScreen);
+
+  let isAltPressed = false;
+
+  window.addEventListener(`keydown`, ({keyCode}) => {
+    if (!isAltPressed) {
+      isAltPressed = keyCode === KEY_CODES.alt;
+    }
+
+    if (isAltPressed) {
+      switch (keyCode) {
+        case KEY_CODES.arrowLeft: {
+          slideToPrevPage();
+          break;
+        }
+
+        case KEY_CODES.arrowRight: {
+          slideToNextPage();
+          break;
+        }
+      }
+    }
+  });
+
+  window.addEventListener(`keyup`, ({keyCode}) => {
+    if (isAltPressed) {
+      isAltPressed = keyCode !== KEY_CODES.alt;
+    }
+  });
+
+  /**
+   * Показывает экран с индексом currentScreen
+   */
+  function showScreen() {
+    mainContainerNode.innerHTML = screenTemplates[currentScreen].innerHTML;
+  }
+
+  /**
+   * Осуществляет переход к предыдущей странице
+   */
+  function slideToPrevPage() {
+    currentScreen = Math.max(0, currentScreen - 1);
+    showScreen();
+  }
+
+  /**
+   * Осуществляет переход к следующей странице
+   */
+  function slideToNextPage() {
+    currentScreen = Math.min(LAST_SCREEN_INDEX, currentScreen + 1);
+    showScreen();
+  }
+}());
