df158e830573b4ae50b57773fd9d60aaa3853dcc
Tatyana Korosteleva <tvkorosteleva@avito.ru>
2017-06-15 00:29:29 +0300

    delete unused constants

--- js/constants.js
+++ js/constants.js
@@ -2,26 +2,6 @@ export const LEVELS_COUNT = 10;
 
 export const LIVES_COUNT = 3;
 
-export const ESTABLISH_YEAR = 2017;
-
-export const SOCIAL_GROUPS = [{
-  name: `Твиттер`,
-  link: `https://twitter.com/htmlacademy_ru`,
-  shortName: `tw`
-}, {
-  name: `Инстаграм`,
-  link: `https://www.instagram.com/htmlacademy/`,
-  shortName: `ins`
-}, {
-  name: `Фэйсбук`,
-  link: `https://www.facebook.com/htmlacademy`,
-  shortName: `fb`
-}, {
-  name: `Вконтакте`,
-  link: `https://vk.com/htmlacademy`,
-  shortName: `vk`
-}];
-
 export const QUESTION_TYPES = {
   chooseType: `chooseType`,
   findPic: `findPic`,
